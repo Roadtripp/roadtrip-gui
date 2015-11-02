e14e9325f933a8cc3c41bebb153a6bd11fd80204
Camille Mauban <camille.mauban@gmail.com>
2015-11-02 18:13:19 -0500

    cities from static json file working but not the events. bubbles do show tho

--- src/js/main.js
+++ src/js/main.js
@@ -6,9 +6,9 @@
 
     })
 
-    // .when('/home', {
-    //   templateUrl: 'admin.html',
-    // })
+    .when('/home', {
+      templateUrl: 'admin.html',
+    })
 
     .when('/timeline', {
       templateUrl: 'timeline.html',
@@ -16,11 +16,35 @@
 
     .when('/selection', {
       templateUrl: 'selection.html',
-    })
+      controller: function($http, $rootScope){
+      $http.get('/api/city-selector.json')
+        .then(function (response){
+          console.log(arguments);
+          $rootScope.cities = response.data.suggested_cities;
+          $rootScope.activities = response.data.suggested_cities.activities;
+        });
+      } // END controller function
+
+    }) // END .when
 
     .when('/start', {
       templateUrl: 'start.html',
     });
 
-  });  // END MODULE
+})  // END MODULE
+
+    // ROUTES TO CREATED TRIP
+    .config(function($routeProvider, $locationProvider){
+      $routeProvider
+        .when('/trip/:id',{
+          templateUrl: 'timeline.html',
+          controller: 'activityController'
+        });
+
+}); // END CONFIG.
+
+
+
+
+
 })(); // END IIFE
