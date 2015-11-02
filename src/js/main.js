4f04ed835ba0c59fd8e381699970071bf2f1000b
Camille Mauban <camille.mauban@gmail.com>
2015-11-02 10:01:37 -0500

    linked up. background header

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+;(function(){
+
+  angular.module('road-Trip', ['ngRoute'], function($routeProvider){
+    $routeProvider.when('/', {
+      templateUrl: 'welcome.html',
+
+    })
+
+    .when('/home', {
+      templateUrl: 'admin.html',
+    })
+
+    .when('/start', {
+      templateUrl: 'start.html',
+    });
+
+  });  // END MODULE
+})(); // END IIFE
